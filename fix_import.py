9c7429890e253dac64f71fe6599c0a76d834e577
andre-assogba <andremarcassogba@gmail.com>
2026-05-03 14:40:37 +0100

    fix: script correction imports complet

--- fix_import.py
+++ fix_import.py
@@ -1,6 +1,9 @@
-with open('/home/andremarcassogba/AZONBOApp/app/serveur.py', 'r') as f:
+path = '/home/andremarcassogba/AZONBOApp/app/serveur.py'
+with open(path) as f:
     lines = f.readlines()
 lines[5] = "from db import initialiser, get_articles_session, get_credit_session, get_produit, get_user_id, lister_produits, ajouter_produit, creer_session, ajouter_vente, lister_sessions, lister_dettes, get_dette, rechercher_dettes, enregistrer_remboursement, lister_remboursements, modifier_vente, modifier_quantites_vente, verifier_utilisateur, get_resume, modifier_produit, supprimer_produit\n"
-with open('/home/andremarcassogba/AZONBOApp/app/serveur.py', 'w') as f:
+lines[6] = "from ventes import Vente\n"
+lines[7] = "from validation import valider_login, valider_inscription, valider_client, valider_paiement, valider_quantite_vente, valider_remboursement, valider_modification_vente\n"
+with open(path, 'w') as f:
     f.writelines(lines)
 print("OK")
